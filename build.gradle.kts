add557945bbfca70b5c2d8622277e4bb67eb36de
anatawa12 <anatawa12@icloud.com>
2023-02-15 16:07:24 +0900

    chore: lock deps for java8 compatiblity

--- build.gradle.kts
+++ build.gradle.kts
@@ -118,6 +118,25 @@ dependencies {
         exclude(group = "cpw.mods",            module = "modlauncher")
     }
 
+    // pin jdt deps
+    // locked means locked due to java 8
+    shade("org.eclipse.jdt:org.eclipse.jdt.core:3.26.0") // locked
+    shade("org.eclipse.platform:org.eclipse.core.commands:3.9.800") // locked
+    shade("org.eclipse.platform:org.eclipse.core.contenttype:3.7.1000") // locked
+    shade("org.eclipse.platform:org.eclipse.core.expressions:3.7.100") // locked
+    shade("org.eclipse.platform:org.eclipse.core.filesystem:1.7.700") // locked
+    shade("org.eclipse.platform:org.eclipse.core.jobs:3.11.0") // locked
+    shade("org.eclipse.platform:org.eclipse.core.resources:3.14.0") // locked
+    shade("org.eclipse.platform:org.eclipse.core.runtime:3.22.0") // locked
+    shade("org.eclipse.platform:org.eclipse.equinox.app:1.5.100") // locked
+    shade("org.eclipse.platform:org.eclipse.equinox.common:3.14.100") // locked
+    shade("org.eclipse.platform:org.eclipse.equinox.preferences:3.9.100") // locked
+    shade("org.eclipse.platform:org.eclipse.equinox.registry:3.10.200") // locked
+    shade("org.eclipse.platform:org.eclipse.osgi:3.18.100")
+    shade("org.eclipse.platform:org.eclipse.text:3.11.0") // locked
+    shade("org.osgi:org.osgi.service.prefs:1.1.2")
+    shade("org.osgi:osgi.annotation:8.1.0")
+
     //Stuff used in the GradleStart classes
     compileOnly("com.mojang:authlib:1.5.16")
     compileOnly("net.minecraft:launchwrapper:1.11"){
